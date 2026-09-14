f69be4bd040f7da4119744ab69def0d60a9d531e
WarrenSCox <76867633+WarrenSCox@users.noreply.github.com>
2026-09-14 19:19:12 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='wozzaworld-v0.15.6';
-const LOCAL=['./','./index.html','./styles.css?v=0.15.6','./app.js?v=0.15.6','./manifest.webmanifest?v=0.15.6','./icon-192.png?v=0.15.6','./icon-192.png?v=0.15.6','./icon-512.png?v=0.15.6','./wozzaworld-logo.png?v=0.15.6'];
+const CACHE='wozzaworld-v0.15.7';
+const LOCAL=['./','./index.html','./styles.css?v=0.15.7','./app.js?v=0.15.7','./manifest.webmanifest?v=0.15.7','./icon-192.png?v=0.15.7','./icon-192.png?v=0.15.7','./icon-512.png?v=0.15.7','./wozzaworld-logo.png?v=0.15.7','./cloud-a.svg?v=0.15.7','./cloud-b.svg?v=0.15.7'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(LOCAL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
